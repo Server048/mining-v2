d3118e2787c2107283821090aec117176fcec0fa
Server048 <64312413+Server048@users.noreply.github.com>
2021-05-10 08:06:45 +0800

    Update Miningv2.sh

--- Miningv2.sh
+++ Miningv2.sh
@@ -6,15 +6,15 @@ cd
 ls
 cd
 sleep 4
-apt update && apt upgrade
-apt install wget
-apt install proot
-apt install git
-apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++
-apt install git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev
+apt update && apt upgrade -y
+apt install wget -y
+apt install proot -y
+apt install git -y
+apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ -y
+apt install git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev -y
 mkdir cpuminer
 cd cpuminer
-apt-get install automake autoconf pkg-config libcurl14-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++
+apt-get install automake autoconf pkg-config libcurl14-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ -y
 git clone https://github.com/tpruvot/cpuminer-multi.git
 cd cpuminer-multi
 ./build.sh
